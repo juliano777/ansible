4a2305c0049f64fcf8477ed934db1a12e0c41c32
Juliano Atanazio <juliano777@gmail.com>
2026-04-21 18:38:09 -0300

    .

--- pg-container-lab/tofu/main.tf
+++ pg-container-lab/tofu/main.tf
@@ -1,34 +1,36 @@
 terraform {
-    required_providers {
-      podman = { source = "containers/podman" }
+  required_providers {
+    podman = {
+      source  = "containers/podman"
     }
+  }
 }
 
-provider "podman" {}
+provider "podman" {
+  uri = "unix:///run/podman/podman.sock"
+}
 
 # Create a PostgreSQL container
-resource "postgresql_container" "postgres" {
-    name = "pg-tofu"
-    image = "docker.io/library/postgres"
+resource "podman_container" "postgres" {
+  name  = "pg-tofu"
+  image = "docker.io/library/postgres:16"
 
-    # Environment variables for PostgreSQL
-    env = {
-        PGUSER = "admin"
-        PGPASSWORD = "123"
-        PGDATABASE = "db_app"
-    }
+  # Environment variables for PostgreSQL
+  env = {
+    POSTGRES_USER     = "admin"
+    POSTGRES_PASSWORD = "123"
+    POSTGRES_DB       = "db_app"
+  }
 
-    # Export PostgreSQL port
-    ports {
-        internal = 5432
-        external = 5432
-    }
+  # Expose PostgreSQL port
+  ports {
+    internal = 5432
+    external = 5432
+  }
 
-    # Persist data using a volume
-    /*
-    volume {
-        host_path = "/tmp/data"
-        container_path = "/var/lib/postgresql/data"
-    }
-    */
+  # Persist data using a volume
+  volume {
+    host_path      = "/tmp/data"
+    container_path = "/var/lib/postgresql/data"
+  }
 }
